860b097c810236edda9d7b2ddd89aee8f5145890
ljiljak <ljiljakaleksandar@gmail.com>
2018-05-15 19:45:22 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import HelloWorld from './components/HelloWorld.vue'
+
+import VueRouter from 'vue-router'
+
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+	{ path: '/', redirect: '/HelloWorld' },
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+}) 
+
 new Vue({
+	router,
   render: h => h(App)
 }).$mount('#app')
